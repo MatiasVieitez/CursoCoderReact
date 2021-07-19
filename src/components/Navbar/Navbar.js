8177fc0d3d516d86d10f7bcc97417e04e87a3dc8
Matias Vieitez <matiasvieitez18@gmail.com>
2021-07-19 16:36:01 -0300

    desafio coder 02 v2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,27 +9,31 @@ const NavBar = () =>
     <>
 
 
-        <Navbar bg="back" variant="dark" sticky="top">
+        <Navbar bg="back" variant="dark" sticky="top" expand="lg" collapseOnSelect>
 
 
 
-            <Navbar.Brand className="brand">
+            <Navbar.Brand className="brand" >
                 <img src={logo} className="logo" />{' '}
                 CreativaKids
             </Navbar.Brand>
 
 
-            <nav className="elements">
-                <Nav.Link className="links" href="#">INICIO</Nav.Link>
-                <Nav.Link className="links" href="#">ACERCA DE MI</Nav.Link>
-                <NavDropdown className="links" title="PRODUCTOS" id="ddl" >
-                    <NavDropdown.Item href="PRODUCTOS/AMIGURUMIS">AMIGURUMIS</NavDropdown.Item>
-                    <NavDropdown.Item href="PRODUCTOS/AMIGURUMIS">ROPA</NavDropdown.Item>
-                    <NavDropdown.Item href="PRODUCTOS/AMIGURUMIS">ACCESORIOS</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link className="links" href="#">CONTACTAME</Nav.Link>
-            </nav>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
 
+
+                <nav className="elements">
+                    <Nav.Link className="links" href="#">INICIO</Nav.Link>
+                    <Nav.Link className="links" href="#">ACERCA DE MI</Nav.Link>
+                    <NavDropdown className="links" title="PRODUCTOS" id="ddl" >
+                        <NavDropdown.Item href="PRODUCTOS/AMIGURUMIS">AMIGURUMIS</NavDropdown.Item>
+                        <NavDropdown.Item href="PRODUCTOS/AMIGURUMIS">ROPA</NavDropdown.Item>
+                        <NavDropdown.Item href="PRODUCTOS/AMIGURUMIS">ACCESORIOS</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link className="links" href="#">CONTACTAME</Nav.Link>
+                </nav>
+            </Navbar.Collapse>
 
 
         </Navbar>
